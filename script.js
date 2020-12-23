219bbed61b56c04fe243b21f018967e9768708ff
Daniel <daniel.granat@op.pl>
2020-12-23 23:20:33 +0100

    Add date validation
    Signed-off-by: Daniel <daniel.granat@op.pl>

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function getDate(date, format) {
-  let dateArr = [...date];
-  let formatArr = [...format];
+  const dateArr = [...date.trim()];
+  const formatArr = [...format.trim()];
 
   function getFilter(formatChar) {
     return (c, idx) => {
@@ -10,17 +10,63 @@ function getDate(date, format) {
     };
   }
 
-  let yearArr = dateArr.filter(getFilter("y"));
-  let monthArr = dateArr.filter(getFilter("m"));
-  let dayArr = dateArr.filter(getFilter("d"));
+  if (!validateDateStr(date, format)) {
+    console.log(`Date str: ${date} is invalid!
+Doesn\'t match to specified format: ${format}`);
+    return;
+  }
 
-  let toStrHandler = (str, c) => str + c;
+  const yearStr = dateArr.filter(getFilter("y")).join("");
+  const monthStr = dateArr.filter(getFilter("m")).join("");
+  const dayStr = dateArr.filter(getFilter("d")).join("");
 
-  let yearStr = yearArr.reduce(toStrHandler);
-  let monthStr = monthArr.reduce(toStrHandler);
-  let dayStr = dayArr.reduce(toStrHandler);
+  const year = Number(yearStr);
+  const month = Number(monthStr) - 1;
+  const day = dayStr.length ? Number(dayStr) : 1;
 
-  return new Date(Number(yearStr), Number(monthStr) - 1, Number(dayStr));
+  if (!validateDate([year, month, day])) {
+    return false;
+  }
+
+  return new Date(year, month, day);
 }
 
-console.log(getDate("2020:12:21", "yyyy:mm:dd"));
+function validateDate(arr) {
+  const [year, month, day] = arr;
+
+  if (month < 0 || month > 11) {
+    console.log("Invalid month!");
+    return false;
+  }
+  if (day < 0 || day > 31) {
+    console.log("Invalid day!");
+    return false;
+  }
+
+  return true;
+}
+
+function validateDateStr(date, format) {
+  dateTrimmed = date.trim();
+  formatTrimmed = format.trim();
+
+  if (dateTrimmed.length !== formatTrimmed.length) {
+    return false;
+  }
+
+  return [...formatTrimmed].reduce((acc, element, idx) => {
+    if (["y", "m", "d"].includes(element)) {
+      if (Number(dateTrimmed[idx]) === NaN) {
+        return false;
+      }
+      return acc;
+    }
+
+    return acc && dateTrimmed[idx] === element;
+  }, true);
+}
+
+const date = getDate("2020:12:30", "yyyy:mm:dd");
+if (date) {
+  console.log(date);
+}

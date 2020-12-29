930f64e4ac55b62e60445f6ca38210f73332b992
Daniel <daniel.granat@op.pl>
2020-12-29 17:01:09 +0100

    Iterators
    Signed-off-by: Daniel <daniel.granat@op.pl>

--- script.js
+++ script.js
@@ -2,19 +2,24 @@ let sym = Symbol("hidden");
 
 let person = {
   name: "Dnaiel",
-  [sym]: "txt",
+  sports: ["skiing", "running", "cycling", "jumping"],
+  [Symbol.iterator]: function () {
+    return {
+      sports: this.sports,
+      i: 0,
+      next: function () {
+        let value = this.sports[this.i];
+        this.i++;
+
+        return {
+          done: this.i > this.sports.length ? true : false,
+          value: value,
+        };
+      },
+    };
+  },
 };
 
-console.log(person);
-console.log(person[sym]);
-const arr = [];
-const fun = function () {};
-const str = "";
-const it = arr[Symbol.iterator];
-
-console.log("typeof Array: " + typeof arr);
-console.log("typeof Function: " + typeof fun);
-console.log("typeof String: " + typeof str);
-
-console.log("Iterator: " + it);
-console.log("typeof Iterator: " + typeof it);
+for (let sport of person) {
+  console.log(sport);
+}

165acfec2e8197d84fc79fa22c5d47c4db6ee889
Daniel <daniel.granat@op.pl>
2020-12-29 16:14:05 +0100

    Check JS Data Types
    Signed-off-by: Daniel <daniel.granat@op.pl>

--- script.js
+++ script.js
@@ -7,3 +7,14 @@ let person = {
 
 console.log(person);
 console.log(person[sym]);
+const arr = [];
+const fun = function () {};
+const str = "";
+const it = arr[Symbol.iterator];
+
+console.log("typeof Array: " + typeof arr);
+console.log("typeof Function: " + typeof fun);
+console.log("typeof String: " + typeof str);
+
+console.log("Iterator: " + it);
+console.log("typeof Iterator: " + typeof it);

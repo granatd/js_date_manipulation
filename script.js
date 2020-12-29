eca86dd8c7868711444893ce5c995ee981c19687
Daniel <daniel.granat@op.pl>
2020-12-29 17:01:45 +0100

    Iterators - simpler
    Signed-off-by: Daniel <daniel.granat@op.pl>

--- script.js
+++ script.js
@@ -4,15 +4,16 @@ let person = {
   name: "Dnaiel",
   sports: ["skiing", "running", "cycling", "jumping"],
   [Symbol.iterator]: function () {
+    let i = 0;
+    const sports = this.sports;
+
     return {
-      sports: this.sports,
-      i: 0,
       next: function () {
-        let value = this.sports[this.i];
-        this.i++;
+        let value = sports[i];
+        i++;
 
         return {
-          done: this.i > this.sports.length ? true : false,
+          done: i > sports.length ? true : false,
           value: value,
         };
       },
